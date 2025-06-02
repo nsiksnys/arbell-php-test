9fbc15092f43f8710543284f9d25f27d4acbaabb
Nadia Siksnys <nadiasiksnys@gmail.com>
2025-06-02 19:46:18 -0300

    Removed doctrine-messenger
    * Removed package symfony/doctrine-messenger
    * Removed the related sql queries in migration Version20250529145057
    * Removed messenger.yaml file in config/packages
    * Removed messenger configuration in .env

--- migrations/Version20250529145057.php
+++ migrations/Version20250529145057.php
@@ -35,22 +35,6 @@ final class Version20250529145057 extends AbstractMigration
           ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
 
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649CCFA12B8 FOREIGN KEY (profile_id) REFERENCES profile (id)');
-
-        //Delete after messenger is uninstalled
-        $this->addSql("CREATE TABLE messenger_messages (
-            id BIGINT AUTO_INCREMENT NOT NULL,
-            body LONGTEXT NOT NULL,
-            headers LONGTEXT NOT NULL,
-            queue_name VARCHAR(190) NOT NULL,
-            created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-            available_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-            delivered_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
-            INDEX IDX_75EA56E0FB7336F0 (queue_name),
-            INDEX IDX_75EA56E0E3BD61CE (available_at),
-            INDEX IDX_75EA56E016BA31DB (delivered_at),
-            PRIMARY KEY(id)
-        )
-        DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB");
     }
 
     public function down(Schema $schema): void
@@ -58,8 +42,5 @@ final class Version20250529145057 extends AbstractMigration
         $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649CCFA12B8');
         $this->addSql('DROP TABLE profile');
         $this->addSql('DROP TABLE user');
-
-        //Delete after messenger is uninstalled
-        $this->addSql('DROP TABLE messenger_messages');
     }
 }
