ae8f8330a1a5a549237325f496c4b724ae9af80e
Nadia Siksnys <nadiasiksnys@gmail.com>
2025-06-03 15:21:17 -0300

    Expose User and Profile
    * Configured User and Profile for GET operations
    * Set normalization and denormalization groups for User and Profile
    * Added save and remove operations in UserRepository

--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -2,18 +2,27 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ProfileRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ProfileRepository::class)]
-class Profile
+#[ApiResource(
+    operations: [ new Get(), new GetCollection() ],
+    normalizationContext: ['groups' => ['user_read', 'profile_read']]
+)]class Profile
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['profile_read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user_read', 'profile_read'])]
     private ?string $role = RoleEnum::USER->value;
 
     public function getId(): ?int
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,27 +2,45 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+    ],
+    normalizationContext: ['groups' => ['user_read']],
+    denormalizationContext: ['groups' => ['user_write']],
+)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user_read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['user_read', 'user_write'])]
     private ?string $email = null;
 
     /**
      * @var Profile The user profile
      */
     #[ORM\ManyToOne(targetEntity: Profile::class)]
+    #[Groups(['user_read', 'user_write'])]
     private Profile $profile;
 
     /**
@@ -32,9 +50,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user_read', 'user_write'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['user_read', 'user_write'])]
     private ?string $phone = null;
 
     public function getId(): ?int
